280660b4819474ae93b7ebc7ca7e940d6ed7428c
CCE107 <yoshbatula2@gmail.com>
2024-02-26 20:22:23 +0800

    DATABASE CONNECTIVITY

--- YUEM DENTAL MANAGEMENT SYSTEM/DENTAL-PROJECT-348460153f0ac81665f6cb6a3ced2b174c9049c3/CCE107/src/main/java/org/example/cce107/dbMain.java
+++ YUEM DENTAL MANAGEMENT SYSTEM/DENTAL-PROJECT-348460153f0ac81665f6cb6a3ced2b174c9049c3/CCE107/src/main/java/org/example/cce107/dbMain.java
@@ -2,6 +2,7 @@ package org.example.cce107;
 
 public class dbMain {
 
+    // This block of code is to know that if the connectivity runs succesfully or failed.
     public static void main(String[] args) {
         dentalbase db = new dentalbase();
         db.dentaldb("postgres", "postgres", "admin");
